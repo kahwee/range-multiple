e8ff4b1dd70003c7ad5ef544fb7017343b5bc4a6
Eng Wan Song <wansong@gmail.com>
2015-05-31 15:06:48 -0400

    Add Handle and Track classes

--- range-multiple.js
+++ range-multiple.js
@@ -3,11 +3,28 @@ window.RangeMultiple = class RangeMultiple {
     this.el = el;
     var div = document.createElement('div');
     div.className = 'rmultiple';
-    div.appendChild(document.createElement('div'));
+    var handle = new Handle(div);
+    var track = new Track(div);
     el.parentNode.insertBefore(div, el.nextSibling);
   }
 
   render() {
 
+  }
+}
+
+class Handle {
+  constructor(el) {
+    var div = document.createElement('div');
+    div.className = 'rhandle';
+    el.appendChild(div);
+  }
+}
+
+class Track {
+  constructor(el) {
+    var div = document.createElement('div');
+    div.className = 'rtrack';
+    el.appendChild(div);
   }
 }

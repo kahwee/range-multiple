4aa44126a0604e8cd140463202872f43598ee647
KahWee Teng <t@kw.sg>
2015-06-01 02:19:52 +0800

    Change indent style to space

--- range-multiple.js
+++ range-multiple.js
@@ -1,14 +1,14 @@
 window.RangeMultiple = class RangeMultiple {
-	constructor(el) {
-		this.el = el;
-		var div = document.createElement('div');
-		div.style.backgroundColor = 'blue';
-		div.style.height = '18px';
-		div.appendChild(document.createElement('div'));
-		el.parentNode.insertBefore(div, el.nextSibling);
-	}
+  constructor(el) {
+    this.el = el;
+    var div = document.createElement('div');
+    div.style.backgroundColor = 'blue';
+    div.style.height = '18px';
+    div.appendChild(document.createElement('div'));
+    el.parentNode.insertBefore(div, el.nextSibling);
+  }
 
-	render() {
+  render() {
 
-	}
+  }
 }

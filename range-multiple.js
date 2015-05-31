bb34e29bfe7206282b473fe0e5699796bd99d855
KahWee Teng <t@kw.sg>
2015-06-01 03:31:42 +0800

    testing options destructing

--- range-multiple.js
+++ range-multiple.js
@@ -1,11 +1,15 @@
 window.rangeMultiple = function(el, opts = {}) {
+  opts.min = opts.min || el.attributes.min.value || undefined;
+  opts.max = opts.max || el.attributes.max.value || undefined;
+  opts.step = opts.step || el.attributes.step.value || undefined;
   return new RangeMultiple(el, opts);
 }
 
 class RangeMultiple {
   constructor(el, {min = 0, max = 100, step = 1}) {
     this.el = el;
-    console.log(el);
+    this.opts = {min, max, step};
+    console.log(el, this.opts);
     var div = document.createElement('div');
     div.className = 'rmultiple';
     var handle = new Handle(div);

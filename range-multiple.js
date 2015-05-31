051ec4da54d65c72cce7b8e256c7046e7679e151
KahWee Teng <t@kw.sg>
2015-06-01 02:14:41 +0800

    Insert an element to test

--- range-multiple.js
+++ range-multiple.js
@@ -1,3 +1,14 @@
-export class RangeMultiple {
+window.RangeMultiple = class RangeMultiple {
+	constructor(el) {
+		this.el = el;
+		var div = document.createElement('div');
+		div.style.backgroundColor = 'blue';
+		div.style.height = '18px';
+		div.appendChild(document.createElement('div'));
+		el.parentNode.insertBefore(div, el.nextSibling);
+	}
 
+	render() {
+
+	}
 }
